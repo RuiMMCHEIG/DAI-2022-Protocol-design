393cc880ff77a576938c4dc7f9a48d14b0f5c9e0
Rui Carneiro <rui.motacarneiro@heig-vd.ch>
2022-11-03 11:54:29 +0100

    [WIP] server implementation
    REFUSED

--- ServerSingleThread/src/main/java/ch/heigvd/api/calc/Server.java
+++ ServerSingleThread/src/main/java/ch/heigvd/api/calc/Server.java
@@ -3,6 +3,7 @@ package ch.heigvd.api.calc;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -12,6 +13,8 @@ import java.util.logging.Logger;
 public class Server {
 
     private final static Logger LOG = Logger.getLogger(Server.class.getName());
+
+    private final int LISTEN_PORT = 1111;
 
     /**
      * Main function to start the server
@@ -27,11 +30,27 @@ public class Server {
      * Start the server on a listening socket.
      */
     private void start() {
-        /* TODO: implement the receptionist server here.
-         *  The receptionist just creates a server socket and accepts new client connections.
-         *  For a new client connection, the actual work is done by the handleClient method below.
-         */
+        LOG.info("Starting server...");
 
+        ServerSocket serverSocket = null;
+
+        LOG.log(Level.INFO, "Creating a server socket and binding it on any of the available interface and on port {0}",
+                new Object[]{Integer.toString(LISTEN_PORT)});
+        try {
+            serverSocket = new ServerSocket(LISTEN_PORT);
+            LOG.log(Level.INFO, "       Local IP address: {0}", new Object[]{serverSocket.getLocalSocketAddress()});
+            LOG.log(Level.INFO, "             Local port: {0}", new Object[]{Integer.toString(serverSocket.getLocalPort())});
+            LOG.log(Level.INFO, "               is bound: {0}", new Object[]{serverSocket.isBound()});
+
+            while (true) {
+                LOG.log(Level.INFO, "Waiting (blocking) for a connection request on {0} : {1}",
+                        new Object[]{serverSocket.getInetAddress(), Integer.toString(serverSocket.getLocalPort())});
+                handleClient(serverSocket.accept());
+            }
+        }
+        catch (IOException ex) {
+            LOG.log(Level.SEVERE, ex.toString(), ex);
+        }
     }
 
     /**
@@ -40,16 +59,74 @@ public class Server {
      * @param clientSocket with the connection with the individual client.
      */
     private void handleClient(Socket clientSocket) {
+        BufferedReader reader = null;
+        BufferedWriter writer = null;
 
-        /* TODO: implement the handling of a client connection according to the specification.
-         *   The server has to do the following:
-         *   - initialize the dialog according to the specification (for example send the list
-         *     of possible commands)
-         *   - In a loop:
-         *     - Read a message from the input stream (using BufferedReader.readLine)
-         *     - Handle the message
-         *     - Send to result to the client
-         */
+        LOG.log(Level.INFO, "A client has arrived. We now have a client socket with following attributes:");
+        LOG.log(Level.INFO, "       Local IP address: {0}", new Object[]{clientSocket.getLocalAddress()});
+        LOG.log(Level.INFO, "             Local port: {0}", new Object[]{Integer.toString(clientSocket.getLocalPort())});
+        LOG.log(Level.INFO, "  Remote Socket address: {0}", new Object[]{clientSocket.getRemoteSocketAddress()});
+        LOG.log(Level.INFO, "            Remote port: {0}", new Object[]{Integer.toString(clientSocket.getPort())});
 
+        LOG.log(Level.INFO, "Getting a Reader and a Writer connected to the client socket...");
+        try {
+            reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), "UTF-8"));
+            writer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream(), "UTF-8"));
+        }
+        catch (IOException ex) {
+            LOG.log(Level.SEVERE, ex.toString(), ex);
+        }
+
+        LOG.log(Level.INFO, "Waiting for client input...");
+        try {
+            String line;
+            if ((line = reader.readLine()) != null) {
+                LOG.log(Level.INFO, line);
+            }
+            //throw new RuntimeException("REQUEST treatment not implement yet");
+            String[] results = line.split(":");
+            if (results.length == 2 && Objects.equals(results[0], "REQUEST")) {
+                //TODO : REQUEST treatment
+            }
+            else {
+                try {
+                    writer.write("REFUSED");
+                    writer.flush();
+                    LOG.log(Level.INFO, "Refused client");
+                }
+                catch (Exception e) {
+                    LOG.log(Level.SEVERE, e.toString(), e);
+                }
+            }
+        }
+        catch(Exception ex) {
+            LOG.log(Level.SEVERE, ex.toString(), ex);
+            try {
+                //TODO : send ERROR:problem instead of REFUSED
+                writer.write("REFUSED");
+                writer.flush();
+                LOG.log(Level.INFO, "Refused client");
+            }
+            catch (Exception e) {
+                LOG.log(Level.SEVERE, e.toString(), e);
+            }
+        }
+        finally {
+            try {
+                if (writer != null) writer.close();
+            } catch (IOException ex) {
+                LOG.log(Level.SEVERE, ex.toString(), ex);
+            }
+            try {
+                if (reader != null) reader.close();
+            } catch (IOException ex) {
+                LOG.log(Level.SEVERE, ex.toString(), ex);
+            }
+            try {
+                if (!clientSocket.isClosed()) clientSocket.close();
+            } catch (IOException ex) {
+                LOG.log(Level.SEVERE, ex.toString(), ex);
+            }
+        }
     }
 }
